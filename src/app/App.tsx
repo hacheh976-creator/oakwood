ad55ad4f3e19f2d1864acf1550e9c590defe7b82
hacheh976-creator <hacheh976@gmail.com>
2026-03-03 02:21:51 +0530

    Update App.tsx

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,15 +1,12 @@
-import { RouterProvider } from "react-router";
+import { useEffect } from "react";
+import { RouterProvider } from "react-router-dom";
 import { AppProvider } from "./contexts/AppContext";
 import { router } from "./routes";
-import { useEffect } from "react";
 import { registerServiceWorker, setupInstallPrompt } from "./utils/pwa";
 
 export default function App() {
   useEffect(() => {
-    // Register service worker for PWA functionality
     registerServiceWorker();
-    
-    // Setup install prompt handler
     setupInstallPrompt();
   }, []);
 
